79af342ed1349aa6f7157f74a194e5bad2051773
trx40 <trm2468@gmail.com>
2022-10-18 10:38:07 +0530

    Add method-override and basic CRUD

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { default: mongoose } = require('mongoose')
 const app = express()
 const path = require('path')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => {
@@ -23,6 +24,9 @@ db.once('open', () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 app.listen(3000, () => {
     console.log("SERVING ON PORT 3000")
 })
@@ -37,12 +41,43 @@ app.get('/makecampground', async (req, res) => {
     res.send(camp)
 })
 
+
+
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new.ejs')
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show.ejs', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, location } = req.body.campground
+    await Campground.findByIdAndUpdate(id, { title, location })
+    res.redirect(`/campgrounds/${id}`)
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit.ejs', { campground })
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Campground(req.body.campground)
+    await newCamp.save()
+    res.redirect(`/campgrounds/${newCamp._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
 })
