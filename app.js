e9bee85ef3a86129bf9bec90bbfa1372938b0ef5
trx40 <trm2468@gmail.com>
2022-10-19 19:46:33 +0530

    Import EJS-Mate in app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const path = require('path')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => {
@@ -21,6 +22,7 @@ db.once('open', () => {
     console.log("Database connected")
 })
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
