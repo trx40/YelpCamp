5fbb529a99bad001242efff30bf93e9dd1c0a33d
trx40 <trm2468@gmail.com>
2022-11-29 19:09:34 +0530

    Add Passportjs on our user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
+
