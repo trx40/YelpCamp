52ba9eecee8f9842df65f3a7fad1524474da5ddb
trx40 <trm2468@gmail.com>
2022-12-02 01:18:14 +0530

    Update users.js
    Refactored register to Signup
    
    Add logout route

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,14 @@ const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync')
 
 router.get('/register', (req, res) => {
-    res.render('users/register.ejs')
+    res.render('users/signup.ejs')
 })
 
 router.get('/login', (req, res) => {
     res.render('users/login.ejs')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/signup', catchAsync(async (req, res) => {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     try {
@@ -25,13 +25,21 @@ router.post('/register', catchAsync(async (req, res) => {
     }
     catch (e) {
         req.flash('error', e.message);
-        res.redirect('/register');
+        res.redirect('/signup');
     }
 
 }))
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
     req.flash('success', "Welcome back!");
+    res.redirect('/campgrounds');
+})
+
+router.get('/logout', (req, res) => {
+    req.logOut((err) => {
+        if (err) return next(err);
+    });
+    req.flash('success', "Logged Out");
     res.redirect('/campgrounds');
 })
 
