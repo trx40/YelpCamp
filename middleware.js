2295e82f2bff80b3d956235d43bd21a3f69e9dce
trx40 <trm2468@gmail.com>
2022-12-03 21:42:15 +0530

    Update middleware.js
    Add validateCampground middleware.
    Add isAuthor middleware.
    Add isReviewAuthor middleware.
    Add validateReview middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Campground = require('./models/campground')
+const Review = require('./models/review')
+const { campgroundSchema, reviewSchema } = require('./validationSchemas')
+const ExpressError = require('./utils/ExpressError')
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -7,3 +12,47 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 }
 
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(element => element.message).join(',')
+        throw new ExpressError(400, msg)
+    } else {
+        next()
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', "Unauthorized user!")
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    else {
+        next()
+    }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', "Unauthorized user!")
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    else {
+        next()
+    }
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(element => element.message).join(',')
+        throw new ExpressError(400, msg)
+    }
+    else {
+        next()
+    }
+}
