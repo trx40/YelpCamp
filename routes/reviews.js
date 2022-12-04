d48885ea3e612841947d107fb9d22d8ea03d7335
trx40 <trm2468@gmail.com>
2022-12-04 15:09:49 +0530

    Update reviews.js
    Use reviews controller to slim down reviews route

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,9 +6,8 @@ const express = require('express')
 // params inside our router file
 const router = express.Router({ mergeParams: true })
 
-const Campground = require('../models/campground')
-const Review = require('../models/review')
-
+// REVIEWS controller
+const reviews = require('../controllers/reviews')
 const catchAsync = require('../utils/catchAsync')
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 
@@ -17,27 +16,11 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware')
 // ****************************************************
 // ******************** POST REVIEW ROUTE *************
 // ****************************************************
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id)
-    const review = new Review(req.body.review)
-    review.author = req.user._id;
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash('success', 'Successfully posted a new review!')
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.postReview))
 
 // ****************************************************
 // ******************** DELELTE REVIEW ROUTE **********
 // ****************************************************
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success', 'Successfully deleted review!')
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router
