fc6b613140ba66189bd5bfc73e56ca9f75ff7b71
trx40 <trm2468@gmail.com>
2022-12-04 15:10:26 +0530

    Update users.js
    Use users controller and router.route method to slim down routes

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,21 @@
 const express = require('express');
 const passport = require('passport');
+
 const router = express.Router();
-const User = require('../models/user');
+
+
+// USERS controller
+const users = require('../controllers/users')
 const catchAsync = require('../utils/catchAsync')
 
-router.get('/register', (req, res) => {
-    res.render('users/signup.ejs')
-})
+router.route('/login')
+    .get(users.renderLoginForm)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), catchAsync(users.logIn))
 
-router.get('/login', (req, res) => {
-    res.render('users/login.ejs')
-})
+router.route('/signup')
+    .get(users.renderSignupForm)
+    .post(catchAsync(users.signUp))
 
-router.post('/signup', catchAsync(async (req, res, next) => {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    try {
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', "Welcome to Yelp Camp!!");
-            res.redirect('/campgrounds');
-        })
-    }
-    catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/signup');
-    }
-
-}))
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), async (req, res) => {
-    req.flash('success', "Welcome back!");
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) return next(err);
-        req.flash('success', "Logged Out");
-        res.redirect('/campgrounds');
-    });
-})
+router.get('/logout', users.logOut)
 
 module.exports = router;
