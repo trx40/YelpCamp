e0fbd7edb4a2350fea82bca76fd2c954651ae521
trx40 <trm2468@gmail.com>
2022-12-09 23:29:37 +0530

    Update campgrounds.js
    Add mapbox forward geocoder - Converts location to coordinates.
    Into create campground function.
    Adds geoJSON object to new campground.
    
    In edit function - Delete images from cloudinary. And remove images from the campground as well.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,10 @@
 const Campground = require('../models/campground')
 const ExpressError = require('../utils/ExpressError')
+const { cloudinary } = require('../cloudinary')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 
 module.exports.index = async (req, res, next) => {
     const campgrounds = await Campground.find({})
@@ -28,6 +33,12 @@ module.exports.editCampground = async (req, res) => {
     const images = req.files.map(f => ({ url: f.path, filename: f.filename }))
     campground.images.push(...images);
     await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${id}`)
 }
@@ -40,9 +51,24 @@ module.exports.renderEditCampgroundForm = async (req, res, next) => {
 }
 
 module.exports.createCampground = async (req, res) => {
-
+    // get geocaode of entered location
+    try {
+        const geoData = await geocoder.forwardGeocode({
+            query: req.body.campground.location,
+            limit: 1
+        }).send();
+    }
+    catch (e) {
+        req.flash('error', e);
+        return;
+    }
+    // create newCamp with information from req
     const newCamp = new Campground(req.body.campground)
+    // add in geometry from mapbox api
+    newCamp.geometry = geoData.body.features[0].geometry;
+    // add images array from req.files array
     newCamp.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    // add author from current user session details
     newCamp.author = req.user._id;
     await newCamp.save();
     req.flash('success', 'Successfully made a new campground!')
