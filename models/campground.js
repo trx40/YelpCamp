488bbf62f631b6b850ef27cce16848d1e08cc71a
trx40 <trm2468@gmail.com>
2022-12-13 13:59:52 +0530

    Update campground model
    Add option to model which will allow us to get a JSON response to virtual method invocation.
    
    Add virtual properties.popUpMarkup - returns a link to the campground which allows to go to a campground from the map itself when we click on an unclustered point

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Review = require('./review')
 
+const opts = { toJSON: { virtuals: true } }
+
 const ImageSchema = new Schema({
     url: String,
     filename: String
@@ -38,7 +40,11 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href=/campgrounds/${this._id}>${this.title}</a><strong><p>${this.description.substring(0, 20)}...</p>`;
+})
 
 CampgroundSchema.post('findOneAndDelete', async (doc) => {
     if (doc) {
