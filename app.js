303690e63b2a508b9e6dc77ace0334d39b1a0850
trx40 <trm2468@gmail.com>
2022-12-14 10:59:17 +0530

    Update app.js
    Initialize express-mongo-sanitize, helmet.
    
    Add name to the session.
    
    In Helmet Set content security policy to false --WIP

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ const ejsMate = require('ejs-mate')
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('passport');
+const mongoSanitize = require('express-mongo-sanitize');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
+const helmet = require('helmet');
 
 const ExpressError = require('./utils/ExpressError')
 
@@ -43,13 +45,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
 
 const sessionConfig = {
+    name: 'yc_session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
@@ -57,7 +62,7 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
